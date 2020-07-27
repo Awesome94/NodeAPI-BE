b2368fffe75423a6230a540143fd18b233b83bb7
awesome94 <suubidavid@gmail.com>
2020-07-27 14:41:14 +0300

    Url prefixes and db connection

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
-
 //  import routes
 const authRoute = require('./routes/auth')
 const actionsRoute = require('./routes/actions')
-
 
 dotenv.config();
 
@@ -17,15 +15,11 @@ mongoose.connect(process.env.DB_CONNECT,
      ()=>console.log("connected to db !")
      );
 
-
 // middleware
 app.use(express.json());
 
 //Route middlewares,
 app.use('/api/v1', authRoute);
 app.use('/api/v1', actionsRoute);
-
-
-
 
 app.listen(3000, ()=>console.log("server up and running"))
